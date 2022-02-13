3daf4863c0578c201dfc8b1f1d3ac7eedb22e3f1
SalihTpc <salihtopcu@hotmail.com.tr>
2022-02-13 17:05:50 +0300

    post add, update and delete func. are added

--- mev/urls.py
+++ mev/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import HomeView, DetailView
+from .views import HomeView, DetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     # path("", views.home, name="home")
     path("", HomeView.as_view(), name="home"),
-    path("blog/<int:pk>", DetailView.as_view(), name="details")
+    path("blog/<int:pk>", DetailView.as_view(), name="details"),
+    path("add_post/", AddPostView.as_view(), name="add_post"),
+    path("edit/<int:pk>", UpdatePostView.as_view(), name="update"),
+    path("delete/<int:pk>", DeletePostView.as_view(), name="delete")
 ]
--- mev/views.py
+++ mev/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from .forms import PostForm, UpdateForm
+from django.urls import reverse_lazy
 
 # def home(request):
 #     return render(request, "home.html")
@@ -8,7 +10,24 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = "home.html"
+    ordering = ["-id"]
     
 class DetailView(DetailView):
     model = Post
     template_name = "details.html"
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm 
+    template_name = "post.html"
+    # fields = "__all__"
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = UpdateForm
+    template_name = "update.html"
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = "delete.html"
+    success_url = reverse_lazy("home")
