899a35941be98697933f7f58ca7b3799013df181
SalihTpc <salihtopcu@hotmail.com.tr>
2022-02-14 01:08:25 +0300

    many changes added.

--- mev/forms.py
+++ mev/forms.py
@@ -8,7 +8,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             # "title": forms.TextInput(attrs={"class":"form-control", "placeholder":"This is Title PlaceHolder"}),
             "title": forms.TextInput(attrs={"class":"form-control"}),
-            "author": forms.Select(attrs={"class":"form-control"}),
+            "author": forms.TextInput(attrs={"class":"form-control", "value":"", "id":"user", "type":"hidden"}),
+            # "author": forms.Select(attrs={"class":"form-control", value: user.id}),
             "body": forms.Textarea(attrs={"class":"form-control"})
         }
 
