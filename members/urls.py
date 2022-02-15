4b455f8b802aa1d6c086742dce33ee1d0fffec4d
SalihTpc <salihtopcu@hotmail.com.tr>
2022-02-15 18:17:50 +0300

    base, login and register pages are edited.

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView
+from .views import UserRegisterView
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
-    path("login/", UserLoginView.as_view(), name="login"),
 ]
 
